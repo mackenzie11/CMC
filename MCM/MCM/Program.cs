b592784ec8ec1c41ee567f41b6a7806f241be07b
mackenzie11 <masimper@msn.com>
2016-01-29 16:41:36 -0700

    Added the code

---
+++ MCM/MCM/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VIKOR
+{
+    class Program
+    {
+
+        //----------------------------------These are change-able constants------------------------------
+        //These are the parameters we are looking at, and their weights (not sure if this is the best way to keep track).
+        public static Dictionary<string, double> paramWeights = new Dictionary<string, double>(){
+            { "OPEID", 0.5 }, {"opeid6", 0.5 }
+        };
+
+        public static double v = 0.5; //This is the weight strategy
+
+        //-------------------------------------------------------------------------------------------------
+
+        //The positions of the parameters columns in the csv file (not sure if we need this after initial reading,
+        //but couuld be helpful)
+        public static Dictionary<string, int> parameters;
+
+        //The school objects, keyed by their name. 
+        public static Dictionary<string, School> schools;
+
+
+        //For each paramter in our parameters, calculates the best value and the worst value
+        //(note that we do not need to know which school achieves these values)
+        //(note 2: how do I distinguish between if a criterion is min-best or max-best??)
+        public static Dictionary<string, double> bestCriterion;
+        public static Dictionary<string, double> worstCriterion;
+
+
+        //Filled once every school has their S and R values calculated 
+        public static Dictionary<School, double> schoolsS;
+        public static Dictionary<School, double> schoolsR;
+        public static Dictionary<School, double> schoolsQ;
+
+
+        public static double Smin;
+        public static double Smax;
+        public static double Rmin;
+        public static double Rmax;
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(readFile(paramWeights.Keys.ToList()));
+            calculateMaxAndMin();
+
+            schoolsS = new Dictionary<School, double>();
+            schoolsR = new Dictionary<School, double>();
+            schoolsQ = new Dictionary<School, double>();
+            foreach (School school in schools.Values)
+            {
+                school.calculateSandR();
+            }
+
+            calculateMaxRandS();
+
+            foreach (School school in schools.Values)
+            {
+                school.calculateQ();
+            }
+            schoolsQ = schoolsQ.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            //Print the first 10
+            for (int i = 0; i < 100; i++)
+            {
+                School s = schoolsQ.ElementAt(i).Key;
+                double value = schoolsQ.ElementAt(i).Value;
+
+                Console.WriteLine(s.name + "," + value);
+            }
+
+            Console.WriteLine(schools.Count);
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// Reads the file and creates all of the schools, adding them to the schools dictionary (do 
+        /// we really want a dictionary?). Takes as input the names of the parameters we want to save.
+        /// Returns true if we have found all the parameters, false otherwise. 
+        /// </summary>
+        /// <param name="paramsWeWant"></param>
+        /// <returns></returns>
+        public static bool readFile(List<string> paramsWeWant)
+        {
+            parameters = new Dictionary<string, int>();
+            schools = new Dictionary<string, School>();
+
+            var reader = new StreamReader(File.OpenRead(@"...\...\test.csv"));
+
+            //First, get the positions of the columns we want
+            var line = reader.ReadLine();
+            var values = line.Split(',');
+            for (int i = 0; i < values.Length; i++)
+            {
+                string currentVal = values[i];
+                if (paramsWeWant.Contains(currentVal))
+                {
+                    parameters.Add(currentVal, i);
+                    paramsWeWant.Remove(currentVal);
+                }
+            }
+
+
+
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                values = line.Split(',');
+
+                Dictionary<string, double> paramValues = new Dictionary<string, double>();
+                foreach (string s in parameters.Keys)
+                {
+                    double paramValue = 0; //Do I need to put a check in, or should I assume it always works?
+                    Double.TryParse(values[parameters[s]], out paramValue);
+                    paramValues.Add(s, paramValue);
+                }
+
+
+                string schoolId = values[0]; //Not sure if this will always be four?
+                School school = new School(schoolId, paramValues);
+                school.name =values[3];
+                schools.Add(schoolId, school);
+            }
+
+
+            return paramsWeWant.Count == 0;
+
+        }
+
+
+        /// <summary>
+        /// Calculate the max and min for each criterion function. 
+        /// Note, I need to adjust for ones where having lower is better!!! (i.e. cost!!)
+        /// </summary>
+        public static void calculateMaxAndMin()
+        {
+            bestCriterion = new Dictionary<string, double>();
+            worstCriterion = new Dictionary<string, double>();
+
+            foreach (string s in paramWeights.Keys)
+            {
+                List<double> temp = new List<double>();
+                foreach (School school in schools.Values)
+                {
+                    temp.Add(school.paramValues[s]); //Add all the schools values to the list
+                }
+
+                //Sort the list, Note: not sure if lowest is first, or what? Need to check!!!
+                temp.Sort();
+                bestCriterion.Add(s, temp.First());
+                worstCriterion.Add(s, temp.Last());
+            }
+
+
+        }
+
+        /// <summary>
+        /// Calculates the special R and S values that we need. 
+        /// </summary>
+        public static void calculateMaxRandS()
+        {
+
+            //List<KeyValuePair<School, double>> SList = schoolsS.ToList();
+
+            //SList.Sort((firstPair, nextPair) =>
+            //{
+            //    return firstPair.Value.CompareTo(nextPair.Value);
+            //}
+            //);
+            //schoolsS = SList.To
+
+            //List<KeyValuePair<School, double>> RList = schoolsR.ToList();
+
+            //RList.Sort((firstPair, nextPair) =>
+            //{
+            //    return firstPair.Value.CompareTo(nextPair.Value);
+            //}
+            //);
+
+            //Check that this works!!
+            schoolsS = schoolsS.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            schoolsR = schoolsR.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            Smin = schoolsS.ElementAt(0).Value;
+            Smax = schoolsS.ElementAt(schoolsS.Count - 1).Value;
+            Rmin = schoolsR.ElementAt(0).Value;
+            Rmax = schoolsR.ElementAt(schoolsR.Count - 1).Value;
+        }
+
+
+    }
+}
---
+++ MCM/MCM/School.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace VIKOR
+{
+    public class School
+    {
+
+        public int id { get; set; }
+        public Dictionary<string, double> paramValues { get; set; }
+        public string name { get; set; }
+
+        public School(string schoolId, Dictionary<string, double> paramValues)
+        {
+            int temp = 0;
+            Int32.TryParse(schoolId, out temp);
+
+            id = temp;
+            this.paramValues = paramValues;
+
+        }
+
+
+
+
+        public void calculateSandR()
+        {
+            double S = 0;
+            List<double> temp = new List<double>();
+
+            foreach (String s in paramValues.Keys)
+            {
+                double weight = Program.paramWeights[s];
+                double best = Program.bestCriterion[s];
+                double worst = Program.worstCriterion[s];
+
+                double total = weight * (best - paramValues[s]) / (best - worst); //Will the signs be correct?
+
+                temp.Add(total);
+                S += total;
+            }
+
+            temp.Sort();
+            double R = temp.Last();
+
+            Program.schoolsR.Add(this, R);
+            Program.schoolsS.Add(this, S);
+        }
+
+
+        public void calculateQ()
+        {
+            double SValue = Program.schoolsS[this];
+            double RValue = Program.schoolsR[this];
+
+            double firstTerm = Program.v * (SValue - Program.Smin) / (Program.Smax - Program.Smin);
+            double secondTerm = (1 - Program.v) * (RValue - Program.Rmin) / (Program.Rmax - Program.Rmin);
+
+            Program.schoolsQ.Add(this, firstTerm + secondTerm);
+        }
+
+
+
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetHashCode() == this.GetHashCode();
+        }
+
+
+        /// <summary>
+        /// Important for use of dictionary.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return id;
+        }
+    }
+}
